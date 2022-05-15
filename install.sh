b1991a6b0e69d3ac7b61ae42c81a3e71a31a7a13
Dilanka Kaushal Hewage <66146701+k4u5h41@users.noreply.github.com>
2022-05-15 18:13:21 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+header()
+{
+echo -e "\n\e[00;31m#########################################################\e[00m" 
+echo -e "\e[00;31m#\e[00m" "\e[00;33mLocal Linux Enumeration & Privilege Escalation Script\e[00m" "\e[00;31m#\e[00m" 
+echo -e "\e[00;31m#########################################################\e[00m" 
+echo -e "\e[00;33m# $version\e[00m\n" 
+
+}
